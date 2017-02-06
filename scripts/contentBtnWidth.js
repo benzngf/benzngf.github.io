6d3459106a027c2bbe5850d61929149acb415874
benzngf <b03902053@ntu.edu.tw>
2017-02-06 17:56:39 +0800

    add content, dynamic btn width feature

---
+++ scripts/contentBtnWidth.js
@@ -0,0 +1,32 @@
+var currentSize = "33%";
+function resizeContentBtn() {
+	var content = document.getElementsByClassName("content");
+	console.log(content[0].offsetWidth);
+	if(content[0].offsetWidth < 600){
+		if(currentSize != "100%"){
+			var elements = document.getElementsByClassName("contentBtn");
+			for (var i = elements.length - 1; i >= 0; i--) {
+				elements[i].style.width = "100%";
+			}
+			currentSize = "100%";
+		}
+	}else if(content[0].offsetWidth < 1024){
+		if(currentSize != "49%"){
+			var elements = document.getElementsByClassName("contentBtn");
+			for (var i = elements.length - 1; i >= 0; i--) {
+				elements[i].style.width = "49%";
+			}
+			currentSize = "49%";
+		}
+	}else{
+		if(currentSize != "33%"){
+			var elements = document.getElementsByClassName("contentBtn");
+			for (var i = elements.length - 1; i >= 0; i--) {
+				elements[i].style.width = "33%";
+			}
+			currentSize = "33%";
+		}
+	}
+}
+resizeContentBtn();
+window.addEventListener("resize", resizeContentBtn);
